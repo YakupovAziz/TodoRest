3aa059f894e8d74a3d64ba7697a9cc5fe2213469
YakupovAziz <asisdes@gmail.com>
2026-02-04 21:05:43 +0500

    Update Spring Boot version 3.5.10 and add security dependency

--- src/main/java/todo/controller/TodoController.java
+++ src/main/java/todo/controller/TodoController.java
@@ -20,13 +20,11 @@ public class TodoController {
         return ResponseEntity.ok(todoService.addTodo(todoDto));
     }
 
-
     @GetMapping
     public ResponseEntity<List<TodoDto>> listTodo() {
         List<TodoDto> todos = todoService.getAllTodos();
         return ResponseEntity.ok(todos);
     }
-
 
     @GetMapping("{id}")
     public ResponseEntity<TodoDto> getTodo(@PathVariable Long id) {
